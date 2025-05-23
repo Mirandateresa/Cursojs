f5049f717536b16cfe378f15a40d08926ebc9f92
Mirandateresa <154276040+Mirandateresa@users.noreply.github.com>
2025-05-22 21:35:50 -0600

    Create 0.4.js

---
+++ 0.4.js
@@ -0,0 +1,16 @@
+//objetos
+ const producto ={
+      nombre : "tablet" ,
+      precio : 300,
+      disponibe: false
+ }
+ //console.log (producto)
+ //console.table(producto)
+
+ //console.log(producto.nombre)
+
+ //Destructuring
+
+ const{nombre} =producto
+ console.log(nombre)
+ console.log ("servidor escuchado :)")
