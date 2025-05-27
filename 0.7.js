f63866dd584bba660a4409601d0783041eafe403
Mirandateresa <154276040+Mirandateresa@users.noreply.github.com>
2025-05-27 12:02:15 -0600

    Create 0.7.js

---
+++ 0.7.js
@@ -0,0 +1,25 @@
+//destructuring de dos o  mas objetos
+
+const producto = {
+    nombre: "Alana",
+    precio: 300,
+    disponibe: false
+}
+
+const clientes = {
+    nombre: "mariel",
+    premium: true,
+}
+
+
+//const carrito = {
+  //  cantidad: 1,
+    //...producto //spred operator
+//}
+ 
+ //crossOriginIsolated.log(carrito.nombre)
+
+
+ const nuevoObjeto = {
+    producto,
+ }
